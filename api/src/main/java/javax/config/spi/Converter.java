2d601fbef9f60bf182c3d2fa5f747529d27e62df
Sebastian Daschner <git@sebastian-daschner.de>
2017-11-28 17:26:51 +0100

    clarified documentation for common-sense converters #13;

--- api/src/main/java/javax/config/spi/Converter.java
+++ api/src/main/java/javax/config/spi/Converter.java
@@ -35,7 +35,7 @@ package javax.config.spi;
  * <p>Converters for the following types are provided by default:
  * <ul>
  *     <li>{@code boolean} and {@code Boolean}, values for {@code true}: (case insensitive)
- *     &quot;true&quot;, &quot;yes&quot;, &quot;Y&quot;, &quot;on&quot;, &quot;1&quot;</li>
+ *     &quot;true&quot;, &quot;yes&quot;, &quot;y&quot;, &quot;on&quot;, &quot;1&quot;</li>
  *     <li>{@code int} and {@code Integer}</li>
  *     <li>{@code long} and {@code Long}</li>
  *     <li>{@code float} and {@code Float}, a dot '.' is used to separate the fractional digits</li>
@@ -44,11 +44,11 @@ package javax.config.spi;
  *     <li>{@code java.time.LocalDateTime} as defined in {@link java.time.LocalDateTime#parse(CharSequence)}</li>
  *     <li>{@code java.time.LocalDate} as defined in {@link java.time.LocalDate#parse(CharSequence)}</li>
  *     <li>{@code java.time.LocalTime} as defined in {@link java.time.LocalTime#parse(CharSequence)}</li>
- *     <li>{@code OffsetDateTime} as defined in {@link java.time.OffsetDateTime#parse(CharSequence)}</li>
- *     <li>{@code OffsetTime} as defined in {@link java.time.OffsetTime#parse(CharSequence)}</li>
- *     <li>{@code Instant}</li>
- *     <li>{@code URL} as defined by {@link java.net.URL#URL(java.lang.String)}</li>
- *
+ *     <li>{@code java.time.OffsetDateTime} as defined in {@link java.time.OffsetDateTime#parse(CharSequence)}</li>
+ *     <li>{@code java.time.OffsetTime} as defined in {@link java.time.OffsetTime#parse(CharSequence)}</li>
+ *     <li>{@code java.time.Instant} as defined in {@link java.time.Instant#parse(CharSequence)}</li>
+ *     <li>{@code java.net.URL} as defined by {@link java.net.URL#URL(String)}</li>
+ *     <li>{@code java.net.URI} as defined by {@link java.net.URI#create(String)}</li>
  * </ul>
  *
  * <p>Custom Converters will get picked up via the {@link java.util.ServiceLoader} mechanism and and can be registered by
@@ -62,30 +62,34 @@ package javax.config.spi;
  * All Built In Converters have a {@code javax.annotation.Priority} of 1
  * A Converter should handle null values returning either null or a valid Object of the specified type.
  *
- * <h3>Implicit Converters</h3>
+ * <h3>Implicit Converter methods</h3>
  *
  * <p>If no explicit Converter and no built-in Converter could be found for a certain type,
- * the {@code Config} provides an <em>Implicit Converter</em>, if</p>
+ * the {@code Config} invokes an <em>Implicit Converter method</em>, if (priority in the given order)</p>
  * <ul>
- *     <li>The target type {@code T} has a Constructor with a String parameter, or</li>
- *     <li>The target type {@code T} has a Constructor with a CharSequence parameter, or</li>
- *     <li>the target type {@code T} has a {@code static T valueOf(String)} method, or</li>
- *     <li>the target type {@code T} has a {@code static T valueOf(CharSequence)} method, or</li>
- *     <li>the target type {@code T} has a {@code static T parse(String)} method, or</li>
- *     <li>the target type {@code T} has a {@code static T parse(CharSequence)} method, or</li>
+ *     <li>The target type {@code T} has a Constructor with a single parameter type that is assignable to {@link CharSequence}, or</li>
+ *     <li>the target type {@code T} has a method {@code static T valueOf(CharSequence)}, with a parameter type that is assignable to {@link CharSequence}, or</li>
+ *     <li>the target type {@code T} has a method {@code static T parse(CharSequence)}, with a parameter type that is assignable to {@link CharSequence}, or</li>
  *     <li>the target type {@code T} is an {@code Enum}, in which case the {@link Enum#valueOf(Class, String)} is being used.</li>
  * </ul>
+ *
+ * If the class of the target type defines several, overloaded <em>Implicit Converter methods</em> that would match the target signature,
+ * for example both {@code static T valueOf(CharSequence)} and {@code static T valueOf(String)}, the implementation will decide which
+ * method is selected (and non-portable behavior results).
  *
  * @author <a href="mailto:rsmeral@apache.org">Ron Smeral</a>
  * @author <a href="mailto:struberg@apache.org">Mark Struberg</a>
  * @author <a href="mailto:emijiang@uk.ibm.com">Emily Jiang</a>
  * @author <a href="mailto:john.d.ament@gmail.com">John D. Ament</a>
+ * @author <a href="mailto:mail@sebastian-daschner.com">Sebastian Daschner</a>
  */
 public interface Converter<T> {
+
     /**
-     * Configure the string value to a specified type
+     * Configure the string value to a specified type.
+     *
      * @param value the string representation of a property value.
-     * @return the converted value or null
+     * @return the converted value or {@code null}.
      *
      * @throws IllegalArgumentException if the value cannot be converted to the specified type.
      */
